498d08f3fa81b4454621529e496cb4e86da6f08b
Stephen Ramsay <rabrandom@gmail.com>
2019-11-20 17:30:57 +0000

    completed several questions from ex b and started ex c

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -56,11 +56,23 @@ users = {
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
 p "Jonathan's Twitter handle is #{users["Jonathan"][:twitter]}."
+
 # 2. Get Erik's hometown
+p "Erik's hometown is #{users["Erik"][:home_town]}."
+
 # 3. Get the array of Erik's lottery numbers
+for item in users["Erik"][:lottery_numbers]
+    p item
+end
+
 # 4. Get the species of Avril's pet Monty
+p "Avril's pet, #{users["Avril"][:pets][0][:name]} is a #{users["Avril"][:pets][0][:species]}."
+
 # 5. Get the smallest of Erik's lottery numbers
+p users["Erik"][:lottery_numbers].min()
+
 # 6. Return an array of Avril's lottery numbers that are even
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "Fluffy"
