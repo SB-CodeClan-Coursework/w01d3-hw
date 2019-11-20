3eba91673b96eac0f59c9e038b4d3c9e72a7a909
Stephen Ramsay <rabrandom@gmail.com>
2019-11-20 15:48:11 +0000

    answered a couple of questions

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -1,8 +1,11 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 #1. Add "Edinburgh Waverley" to the end of the array
+stops << "Edinburgh Waverley"
 #2. Add "Glasgow Queen St" to the start of the array
+stops.unshift(Glasgow Queen Street)
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+
 #4. Print out the index position of "Linlithgow"
 #5. Remove "Livingston" from the array using its name
 #6. Delete "Cumbernauld" from the array by index
@@ -10,3 +13,4 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 #8. Show as many ways as you can to return "Falkirk High" from the array?
 #9. Reverse the positions of the stops in the array
 #10 Print out all the stops using a for loop
+p stops
